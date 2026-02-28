a2abf9ad1407c42589a586f7752d1c7dbf5c0f42
Yashavanath <yashvanthyuvi8055@gmail.com>
2026-02-28 11:35:48 +0530

    update/local

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
   // Deploy to https://yashavanth-l.github.io/Leeso/
-  basePath: '/Leeso',
-  assetPrefix: '/Leeso',
+  basePath: isProd ? '/Leeso' : '',
+  assetPrefix: isProd ? '/Leeso' : '',
 };
 
 module.exports = nextConfig;
